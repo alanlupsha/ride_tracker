85e96e7fb95d83320061d1abfafc3a7dcd218f7c
Alan Lupsha <alanlupshadeveloper@gmail.com>
2019-01-07 03:26:50 -0500

    added jdbcTemplate

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,23 +1,27 @@
 package com.pluralsight.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.pluralsight.model.Ride;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.pluralsight.model.Ride;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
 
-	@Override
-	public List<Ride> getRides() {
-		Ride ride = new Ride();
-		ride.setName("Corner Canyon");
-		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
-		rides.add(ride);
-		return rides;
-	}
-	
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Ride> getRides() {
+        Ride ride = new Ride();
+        ride.setName("Corner Canyon");
+        ride.setDuration(120);
+        List<Ride> rides = new ArrayList<>();
+        rides.add(ride);
+        return rides;
+    }
+
 }
