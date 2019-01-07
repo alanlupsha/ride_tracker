b15995bcbe77b66a41cff26c45288fb5d11502d1
Alan Lupsha <alanlupshadeveloper@gmail.com>
2019-01-07 03:46:45 -0500

    added create record option

--- src/main/java/com/pluralsight/service/RideService.java
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -1,11 +1,13 @@
 package com.pluralsight.service;
 
-import java.util.List;
-
 import com.pluralsight.model.Ride;
+
+import java.util.List;
 
 public interface RideService {
 
-	List<Ride> getRides();
+    Ride createRide(Ride ride);
+
+    List<Ride> getRides();
 
 }
